2f11303d7db4e0d69f5e27a9872ce5e821c52f48
alayzcer <alayzcer@gmail.com>
2023-03-28 19:50:09 +0300

    add cli

---
+++ brain_games/cli.py
@@ -0,0 +1,12 @@
+import prompt
+
+def welcome_user():
+    """ user's greeting
+    """
+    while True:
+        name = prompt.string('May I have your name? ').strip()
+        # skip only spaces
+        if 0 == len(name):
+            continue
+        print(f"Hello, {name}!")
+        break
