3681d11908f88b0f5e665f9b1ed3e6e52441e179
alayzcer <alayzcer@gmail.com>
2023-04-07 19:02:26 +0300

    remove get user's answer

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -7,9 +7,3 @@ def welcome_user():
     name = prompt.string('May I have your name? ', False).strip()
     print(f"Hello, {name}!")
     return name
-
-
-def get_user_answer_on_question(value):
-    print("Question:", value.strip())
-    answer = prompt.string('Your answer: ', False).strip()
-    return answer
--- brain_games/game_cycle.py
+++ brain_games/game_cycle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import prompt
 from brain_games import cli
 
 
@@ -9,8 +10,9 @@ def run(core):
     game_steps_count = 3
     for _ in range(game_steps_count):
         step = core["make_step"]()
-        question = core["get_question"](step)
-        user_answer = cli.get_user_answer_on_question(question)
+        question = core["get_question"](step).strip()
+        print("Question:", question)
+        user_answer = prompt.string('Your answer: ', False).strip()
         if not core["check_user_answer"](step, user_answer):
             right_answer = core["get_right_answer"](step)
             print(f"'{user_answer}' is wrong answer ;(. "
