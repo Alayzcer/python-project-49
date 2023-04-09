e1c220b0b68b5d5e2f06a2536586ede7fbe339dd
alayzcer <alayzcer@gmail.com>
2023-04-09 19:48:08 +0300

    rename no in even game

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,8 +6,8 @@ DESCRIPTION = ('Answer "yes" if the number '
 
 
 def make_step():
-    no = random.randrange(1, 345)
-    question = str(no)
-    no_is_even = no % 2 == 0
+    random_number = random.randrange(1, 345)
+    question = str(random_number)
+    no_is_even = random_number % 2 == 0
     answer = "yes" if no_is_even else "no"
     return [question, answer]
