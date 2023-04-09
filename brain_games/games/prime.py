a33a4f2e2a4a61009d3f493b323b25ccf7854839
alayzcer <alayzcer@gmail.com>
2023-04-09 08:45:28 +0300

    update is_prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,13 +4,13 @@ DESCRIPTION = ('Answer "yes" if given number '
                'is prime. Otherwise answer "no".')
 
 
-def is_prime(value):
-    if value <= 1:
+def is_prime(number):
+    if number <= 1:
         return False
-    if 2 == value:
+    if 2 == number:
         return True
-    for i in range(3, value):
-        if 0 == value % i:
+    for i in range(3, number):
+        if 0 == number % i:
             return False
     return True
 
