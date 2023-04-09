32b86f634289faab02919dc3a6148faad2f0a6d1
alayzcer <alayzcer@gmail.com>
2023-04-09 08:30:26 +0300

    remove operand const in calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,7 +6,6 @@ OPERATORS = [
     ["-", lambda a, b: a - b],
     ["*", lambda a, b: a * b],
 ]
-OPERAND_LIMIT = 30
 
 
 def make_step():
@@ -14,8 +13,8 @@ def make_step():
     """
     op_id = random.randrange(0, len(OPERATORS))
     name, just_do_it = OPERATORS[op_id]
-    a = random.randrange(1, OPERAND_LIMIT)
-    b = random.randrange(1, OPERAND_LIMIT)
+    a = random.randrange(1, 30)
+    b = random.randrange(1, 30)
     question = f"{a} {name} {b}"
     answer = just_do_it(a, b)
     return [question, str(answer)]
