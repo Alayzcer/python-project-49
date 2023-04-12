64cf9794185783749a6eca40b9e863eb95c4482d
alayzcer <alayzcer@gmail.com>
2023-04-12 18:10:36 +0300

    remove modify user input

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,5 +4,5 @@ import prompt
 def welcome_user():
     """ user's greeting
     """
-    name = prompt.string('May I have your name? ', False).strip()
+    name = prompt.string('May I have your name? ', False)
     print(f"Hello, {name}!")
